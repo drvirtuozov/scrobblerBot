6328d04e294fc7efcf8d9a93974349fee5a73d8e
drvirtuozov <dr.virtuozov@ya.ru>
2017-01-05 10:59:32 +0300

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = mongoose.Schema({
+  _id: { type: Number, required: true },
+  username: String,
+  key: String,
+  account: String,
+  token: String,
+  scrobbles: { type: Number, default: 0 },
+  timestamp: { type: Number, default: Date.now },
+  lastScrobble: Number,
+  album: {
+    title: String,
+    artist: String,
+    tracks: [{
+      name: String,
+      duration: { 
+        type: Number, 
+        default: 300, 
+        set: function(duration) { 
+          duration = duration || 300;
+          return duration < 30 ? 30 : duration; 
+        }
+      }
+    }]
+  },
+  track: {
+    name: String,
+    artist: String,
+    album: String
+  },
+  discogsResults: Array
+}, {collection: "scrobbler-users"});
+
+module.exports = mongoose.model("User", userSchema);
