34d51990db53255b320fc544d64899c5bfbbb4bb
drvirtuozov <dr.virtuozov@ya.ru>
2017-01-18 06:22:08 +0300

    Add to-title-case library

--- src/models/user_album.js
+++ src/models/user_album.js
@@ -1,12 +1,23 @@
 import db from '../db';
+import toTitleCase from 'to-title-case';
 
 
 const UserAlbum = db.import('user_album', (db, DataTypes) => {
   const { STRING } = DataTypes;
 
   return db.define('user_album', {
-    title: STRING,
-    artist: STRING
+    title: {
+      type: STRING,
+      set: function(value) {
+        this.setDataValue('title', toTitleCase(value));
+      }
+    },
+    artist: {
+      type: STRING,
+      set: function(value) {
+        this.setDataValue('artist', toTitleCase(value));
+      }
+    }
   }, { 
     underscored: true,
     timestamps: false
