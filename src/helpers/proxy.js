601f831601bfaa45de810b01ee0f75e527275d2e
drvirtuozov <dr.virtuozov@ya.ru>
2017-05-16 05:26:06 +0300

    Add proxy checker

--- src/helpers/proxy.js
+++ src/helpers/proxy.js
@@ -1,19 +1,21 @@
 const proxyLists = require('proxy-lists');
+const axios = require('axios');
+const config = require('../../config');
 
 
 let uncheckedProxies = [];
+let checkedProxies = [];
 
 async function getUncheckedProxies() {
-  const results = [];
+  const result = [];
   const gettingProxies = proxyLists.getProxies({
-    countries: null,
     protocols: ['http'],
     ipTypes: ['ipv4'],
   });
 
   return new Promise((resolve) => {
     gettingProxies.on('data', (proxies) => {
-      results.push(...proxies);
+      result.push(...proxies);
     });
 
     gettingProxies.on('error', (err) => {
@@ -21,23 +23,38 @@ async function getUncheckedProxies() {
     });
 
     gettingProxies.once('end', () => {
-      console.log('Done getting proxies\n');
-      resolve(results);
+      console.log(`Done getting proxies. ${result.length} proxies found`);
+      resolve(result);
     });
   });
 }
 
-getUncheckedProxies()
-  .then((proxies) => {
-    uncheckedProxies = proxies;
-  })
-  .catch((err) => {
-    console.log('Getting unchecked proxies error:', err.message);
+async function getCheckedProxies() {
+  let checkedCount = 0;
+  const result = [];
+
+  return new Promise((resolve) => {
+    uncheckedProxies.forEach((proxy) => {
+      axios.post(config.LASTFM_URL, null, {
+        timeout: 3000,
+      })
+        .catch((err) => {
+          if (err.response && err.response.status === 400) {
+            result.push(proxy);
+          }
+
+          if (++checkedCount === uncheckedProxies.length) {
+            console.log('Proxies checked. Working count:', result.length);
+            resolve(result);
+          }
+        });
+    });
   });
+}
 
 function getRandomChekedProxy() {
-  if (uncheckedProxies.length) {
-    const proxy = uncheckedProxies[Math.floor(Math.random() * uncheckedProxies.length)];
+  if (checkedProxies.length) {
+    const proxy = checkedProxies[Math.floor(Math.random() * checkedProxies.length)];
     return {
       host: proxy.ipAddress,
       port: proxy.port,
@@ -49,6 +66,23 @@ function getRandomChekedProxy() {
     port: null,
   };
 }
+
+getUncheckedProxies()
+  .then((proxies) => {
+    uncheckedProxies = proxies;
+  })
+  .then(() => getCheckedProxies())
+  .then((result) => {
+    checkedProxies = result;
+  });
+
+setInterval(async () => {
+  checkedProxies = await getCheckedProxies();
+}, 600000); // every 10 minutes
+
+setInterval(async () => {
+  uncheckedProxies = await getUncheckedProxies();
+}, 3600000); // every 1 hour
 
 module.exports = {
   getRandomChekedProxy,
--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -4,7 +4,6 @@ const { getRandomChekedProxy } = require('./proxy');
 
 async function proxyPost(url, data) {
   const proxy = getRandomChekedProxy();
-  console.log('Request via', proxy.host, proxy.port);
   return axios.post(url, data, {
     proxy: proxy.host ? proxy : null,
   });
