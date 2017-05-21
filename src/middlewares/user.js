9c4f6007d228dcc93ca96077240254dd539a8bc0
drvirtuozov <dr.virtuozov@ya.ru>
2017-05-21 11:13:33 +0300

    Wrap user middleware in try/catch

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,8 +1,13 @@
 const { findUserById } = require('../helpers/dbmanager');
+const { error } = require('../helpers/utils');
 
 
 module.exports = async (ctx, next) => {
-  const user = await findUserById(ctx.from.id);
-  ctx.user = user;
-  next();
+  try {
+    const user = await findUserById(ctx.from.id);
+    ctx.user = user;
+    next();
+  } catch (e) {
+    error(ctx, e);
+  }
 };
