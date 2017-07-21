b1d0a1fc31cbb29cfe951d66069e9133f654734c
drvirtuozov <dr.virtuozov@ya.ru>
2017-07-21 21:35:06 +0300

    Add session middleware

---
+++ src/middlewares/session.js
@@ -0,0 +1,17 @@
+const RedisSession = require('telegraf-session-redis');
+const config = require('../../config');
+
+
+const session = new RedisSession({
+  store: {
+    url: config.REDIS_URL,
+  },
+  getSessionKey(ctx) {
+    return ctx.from.id;
+  },
+})
+
+module.exports = {
+  session,
+  sessionMiddleware: session.middleware(),
+};
