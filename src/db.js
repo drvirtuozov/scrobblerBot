8b5ed8cdcbac8bb14f6fd45c04cc994cbaab9a06
drvirtuozov <dr.virtuozov@ya.ru>
2017-07-25 19:07:26 +0300

    Add some db conn options

--- src/db.js
+++ src/db.js
@@ -6,16 +6,28 @@ const config = require('../config');
 const db = mongoose.connection;
 
 mongoose.Promise = Promise;
-mongoose.connect(config.MONGODB_URL)
-  .catch((err) => {
-    console.log('Database connection error:', err.message);
-  });
+mongoose.connect(config.MONGODB_URL, {
+  useMongoClient: true,
+  keepAlive: 1,
+  connectTimeoutMS: 10000,
+}).catch(() => {});
+
+db.on('connecting', () => {
+  console.log('Connecting to database...');
+});
+
+db.on('disconnected', () => {
+  console.log('Disconnected from database');
+});
 
 db.on('error', (err) => {
   console.log('Failed to connect to database server:', err.message);
+  setTimeout(() => {
+    db.openUri(config.MONGODB_URL).catch(() => {});
+  }, 10000);
 });
 
-db.once('open', () => {
+db.on('open', () => {
   console.log('Connection to database established!');
 });
 
