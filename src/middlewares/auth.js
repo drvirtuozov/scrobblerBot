e04ce79203849d09f3b2b69bc77b991a2b9854e6
drvirtuozov <dr.virtuozov@ya.ru>
2017-07-27 18:04:16 +0300

    Fix auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,5 +5,5 @@ const { isUserAuthorized } = require('../helpers/utils');
 module.exports = branch(isUserAuthorized, (ctx, next) => {
   next();
 }, (ctx) => {
-  ctx.flow.enter('auth');
+  ctx.enterScene('auth');
 });
