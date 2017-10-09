dfe67328c2d95380ae697d1a0f4feee8378a73e8
drvirtuozov <drvirtuozov@ya.ru>
2017-10-09 14:08:21 +0300

    Change logger format

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,6 +1,8 @@
 const TelegrafLogger = require('telegraf-logger');
 
 
-const logger = new TelegrafLogger();
+const logger = new TelegrafLogger({
+  format: '%updateType => *%sceneId* @%username %firstName %lastName (%fromId): <%updateSubType> %content',
+});
 
 module.exports = logger.middleware();
