ec733fb8b6c65f4671b29892cca3bdbec3a874e5
drvirtuozov <drvirtuozov@ya.ru>
2017-11-02 07:50:05 +0300

    Fix user middleware

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -2,6 +2,11 @@ const { findUserById } = require('../helpers/dbmanager');
 
 
 module.exports = async (ctx, next) => {
+  if (ctx.inlineQuery) {
+    await next();
+    return;
+  }
+
   ctx.user = await findUserById(ctx.from.id);
   await next();
 };
