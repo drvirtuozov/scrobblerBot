d65f99d3fa689c56b6dba71093c557e3e5e25943
drvirtuozov <drvirtuozov@ya.ru>
2017-11-11 17:44:18 +0300

    Ignore some cb queries in user middleware

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -5,6 +5,14 @@ module.exports = async (ctx, next) => {
   if (ctx.inlineQuery) {
     await next();
     return;
+  } else if (ctx.callbackQuery) {
+    switch (ctx.callbackQuery.data) {
+      case 'CANCEL':
+      case 'REPEAT':
+        await next();
+        return;
+      default: // pass
+    }
   }
 
   ctx.user = await findUserById(ctx.from.id);
