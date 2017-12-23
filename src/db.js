d23f910151fe7d22c272aec1515c762a9cc5d6ed
drvirtuozov <drvirtuozov@ya.ru>
2017-12-23 23:39:16 +0300

    Change db conn settings

--- src/db.js
+++ src/db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-const config = require('../config');
+const { MONGODB_URL } = require('../config');
 
 
 const db = mongoose.connection;
 
 mongoose.Promise = Promise;
-mongoose.connect(config.MONGODB_URL, {
+mongoose.connect(MONGODB_URL, {
   useMongoClient: true,
-  keepAlive: 1,
-  connectTimeoutMS: 10000,
-}).catch(() => {});
+  keepAlive: true,
+  autoReconnect: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 1000,
+});
 
 db.on('connecting', () => {
   console.log('Connecting to database...');
@@ -19,11 +21,12 @@ db.on('disconnected', () => {
   console.log('Disconnected from database');
 });
 
+db.on('disconnected', () => {
+  console.log('Reconnected to database');
+});
+
 db.on('error', (err) => {
-  console.log('Failed to connect to database server:', err.message);
-  setTimeout(() => {
-    db.openUri(config.MONGODB_URL).catch(() => {});
-  }, 10000);
+  console.log('Failed to connect to database:', err.message);
 });
 
 db.on('open', () => {
